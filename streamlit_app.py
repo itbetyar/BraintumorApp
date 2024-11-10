94f41120e6c5541ec1f7fb72b33ee91f1ea49032
IT Betyár <145707654+itbetyar@users.noreply.github.com>
2024-11-11 00:48:13 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,20 @@ st.subheader("I am a subheader!")
 
 st.text(" Ez egy sima **text**, mint ilyen a csillagok nem \"bold-olnak\" semmit")
 
+col1,col2,col3,col4,col5,col6 = st.columns([1,1,1,1,1,1])
+col1.image("imgs/out_test01.jpg", width=100)
+col1.button("Minta 1", on_click=myaction1, key="kisgomb1")
+col2.image("imgs/out_test02.jpg", width=100)
+col2.button("Minta 2", on_click=myaction2, key="kisgomb2")
+col3.image("imgs/out_test03.jpg", width=100)
+col3.button("Minta 3", on_click=myaction3, key="kisgomb3")
+col4.image("imgs/out_test04.jpg", width=100)
+col4.button("Minta 4", on_click=myaction4, key="kisgomb4")
+col5.image("imgs/out_test06.jpg", width=100)
+col5.button("Minta 5", on_click=myaction5, key="kisgombn5", help="Negatív, tumor mentes minta")
+col6.image("imgs/out_test07.jpg", width=100)
+col6.button("Minta 6", on_click=myaction6, key="kisgombn6", help="Negatív, tumor mentes minta")
+
 
 st.markdown("---")
 st.markdown("# Markdown 1 Header")
